1e7402f79c0500d20402a6529554d930a36e7c2d
Tien <lodkat01@gmail.com>
2015-10-13 12:30:06 +0300

    Only ffmpeg optimization

--- lib/av.rb
+++ lib/av.rb
@@ -4,26 +4,25 @@ require "av/logger"
 require "av/cli"
 require "av/param_hash"
 require "av/commands/ffmpeg"
-require "av/commands/avconv"
 require "cocaine"
 require "av/engine" if defined?(Rails)
 
 module Av
   extend self
   extend Logger
-  
+
   def options
     @options ||= {
       log: true,
       quiet: true,
     }
   end
-  
+
   def cli(options = {})
     @options = options unless options.empty?
     ::Av::Cli.new(options)
   end
-  
+
   def run line, codes = [0]
     ::Av.log("Running command: #{line}")
     begin
